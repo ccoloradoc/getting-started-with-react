de845fee841047261c18f54764e104c30805da42
Cristian Colorado <cristian.colorado@hbr.org>
2018-07-24 18:21:39 -0500

    s9 - Step 9

--- js/app.js
+++ js/app.js
@@ -1,26 +1,57 @@
+// Actions
+const FETCH_ITEMS = 'FETCH_ITEMS';
+
+function fetchItems() {
+  const request = axios.get('/api/v1/items');
+
+  return {
+    type: FETCH_ITEMS,
+    payload: request
+  }
+}
+
+//Reducer
+export function ItemsReducer(state = [], action) {
+  switch (action.type) {
+    case FETCH_USERS:
+      return _.mapKeys(action.payload.data, 'id');
+    case UPDATE_USER:
+      // TODO: Implement element update
+      return state;
+    case DELETE_USER:
+      // TODO: Implement element deletiin
+    default:
+      return state;
+  }
+}
+
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = { items: [] };
+    componentDidMount() {
+      this.props.fetchItems();
     }
 
     render() {
         return (
           <div className="row">
-            <ItemList items={this.state.items}/>
+            <ItemList items={this.props.items}/>
             <ItemForm onSubmit={ this.onItemSubmited.bind(this) }/>
           </div>
         );
     }
 
     onItemSubmited(item) {
-      this.setState({
-        items: this.state.items.concat(item)
-      });
+      this.props.addItem(item);
     }
 }
 
+// Wrap App with Redux allowing to access Redux Model
+var ReduxApp =  connect((state) => {
+  return {
+    items: state.items
+  };
+}, { fetchItems, addItem })(App);
+
 ReactDOM.render(
-    <App/>,
+    <ReduxApp/>,
     document.getElementById("root")
 );
